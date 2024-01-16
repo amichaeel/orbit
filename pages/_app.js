7bc3cac191f5b15c38aa015122604b93866fdd64
amichaeel <mail@anthonymham.com>
2024-01-15 22:14:33 -0500

    added meta info

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import { UserContextProvider } from '../context/userContext';
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
@@ -8,9 +9,20 @@ config.autoAddCss = false
 function MyApp({ Component, pageProps }) {
 
   return (
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <title>Orbit</title>
+      </Head>
       <UserContextProvider>
         <Component {...pageProps} />
       </UserContextProvider>
+    </>
   );
 }
 
